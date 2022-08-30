0b854e700fb77e5b6602a5b72c1c15a90862a0f2
orionaskatu <100234619+orionaskatu@users.noreply.github.com>
2022-08-30 14:00:47 +0200

    Update discord.sh

--- discord.sh
+++ discord.sh
@@ -5,9 +5,15 @@
 url="https://discord.com/api/webhooks/XXX/XXX"
 # Bot username that will send the message
 username="stable-diffusion"
+# Cleanup unwanted characters when prompt is too long
+content=$(echo ${1} | tr -d '\n();')
 # Create json structure
-json=$(jo username="${username}" content="${1}")
+json=$(jo username="${username}" content="${content}")
 # Get escaped filename
 image=$(ls ${2})
 
+# Silently send the payload to Discord api
 curl -o /dev/null -s -F "payload_json=${json}" -F "file=@\"${image}\"" "${url}"
+
+# For debugging
+#curl -F "payload_json=${json}" -F "file=@\"${image}\"" "${url}"
