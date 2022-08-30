adc4f853cb9676a3e54b3a3de60b68b26e1f71bd
orionaskatu <100234619+orionaskatu@users.noreply.github.com>
2022-08-30 14:38:58 +0200

    Update discord.sh

--- discord.sh
+++ discord.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Needs curl & jo packages
+# Needs curl, jo and jpegoptim packages
+#apt -y install curl jo jpegoptim
 
 # Discord webhook url
 url="https://discord.com/api/webhooks/XXX/XXX"
@@ -11,6 +12,14 @@ content=$(echo ${1} | tr -d '\n();')
 json=$(jo username="${username}" content="${content}")
 # Get escaped filename
 image=$(ls ${2})
+
+# Reduce file size to 7,5MB (Discord's max allowed size is 8MB)
+if [[ $(stat -c %s ${image}) -gt 7700000 ]]
+then
+    # 8MB quiet strip all markers
+    jpegoptim -S7500 -qs ${image}
+fi
+
 
 # Silently send the payload to Discord api
 curl -o /dev/null -s -F "payload_json=${json}" -F "file=@\"${image}\"" "${url}"
