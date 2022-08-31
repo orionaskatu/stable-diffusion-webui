02d4cad874ff764014396ed9b7fb281e86ecc5a1
orionaskatu <100234619+orionaskatu@users.noreply.github.com>
2022-08-31 22:32:45 +0200

    fix discord upload

--- discord.sh
+++ discord.sh
@@ -6,23 +6,20 @@
 url="https://discord.com/api/webhooks/XXX/XXX"
 # Bot username that will send the message
 username="stable-diffusion"
-# Cleanup unwanted characters when prompt is too long
-content=$(echo ${1} | tr -d '\n();')
 # Create json structure
-json=$(jo username="${username}" content="${content}")
+json=$(jo username="${username}" content="${1}")
 # Get escaped filename
 image=$(ls ${2})
 
 # Reduce file size to 7,5MB (Discord's max allowed size is 8MB)
 if [[ $(stat -c %s ${image}) -gt 7700000 ]]
 then
+    # Convert to JPG
+    mogrify -format jpg ${image}
+    image=${image%.png}.jpg
     # 8MB quiet strip all markers
     jpegoptim -S7500 -qs ${image}
 fi
 
-
 # Silently send the payload to Discord api
-curl -o /dev/null -s -F "payload_json=${json}" -F "file=@\"${image}\"" "${url}"
-
-# For debugging
-#curl -F "payload_json=${json}" -F "file=@\"${image}\"" "${url}"
+curl -o /dev/null -s --form-string "payload_json=${json}" -F "file=@\"${image}\"" "${url}"
