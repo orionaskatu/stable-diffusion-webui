596382d01be8fac898ed325bdfe370e76d3140b7
orionaskatu <100234619+orionaskatu@users.noreply.github.com>
2022-09-02 15:12:56 +0200

    add colors to install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 # Pretty print
-delimiter="\n##############################################\n"
-
-# Target directory without trailing slash
-if [[ -z ${1} ]]
-then
-    target="/home/${username}"
-else
-    target="${1}"
-fi
+delimiter="\n###################################################\n"
 
 printf ${delimiter}
 printf "Install script for stable-diffusion + Web UI"
@@ -20,21 +12,32 @@ uid=$(id -u)
 if [[ ${uid} -eq 0 ]]
 then
     printf ${delimiter}
-    printf -"ERROR: This script must not be launched as root, aborting..."
+    printf "\x1B[31mERROR: This script must not be launched as root, aborting...\e[0m"
     printf ${delimiter}
     exit 1
 fi
 
+# Target directory without trailing slash
+if [[ -z ${1} ]]
+then
+    target="/home/${username}"
+else
+    target="${1}"
+fi
+printf ${delimiter}
+printf "Target directory will be : \x1B[32m${target}/\e[0m"
+printf ${delimiter}
+
 username=$(whoami)
 printf ${delimiter}
-printf "Running on ${username} user"
+printf "Running on \x1B[32m${username}\e[0m user"
 printf ${delimiter}
 
 # Check if passwordless sudo
 if ! sudo -l | grep -q NOPASSWD
 then
     printf ${delimiter}
-    printf "${username} cannot passwordless sudo, aborting..."
+    printf "\x1B[31mERROR: ${username} cannot passwordless sudo, aborting...\e[0m"
     printf ${delimiter}
     exit 1
 fi
@@ -45,10 +48,10 @@ printf ${delimiter}
 sudo apt update && sudo apt -y install curl unzip git python3 python3-pip jo jpegoptim mitmproxy
 
 printf ${delimiter}
-printf "Create and change directory to ${target}/diffusion/"
+printf "Create and change directory to \x1B[32m${target}/diffusion/\e[0m"
 printf ${delimiter}
 mkdir "${target}"/diffusion
-cd "${target}"/diffusion/ || { printf "Can't cd to ${target}/diffusion/, aborting..."; exit 1; }
+cd "${target}"/diffusion/ || { printf "\x1B[31mERROR: Can't cd to ${target}/diffusion/, aborting...\e[0m"; exit 1; }
 
 printf ${delimiter}
 printf "Clone basujindal's optimized stable-diffusion fork"
@@ -61,7 +64,7 @@ printf ${delimiter}
 git clone https://github.com/orionaskatu/stable-diffusion-webui.git
 
 printf ${delimiter}
-printf "Install or upgrade Miniconda in ~/miniconda"
+printf "Install or upgrade Miniconda in \x1B[32m${target}/miniconda\e[0m"
 printf ${delimiter}
 curl -s https://gist.githubusercontent.com/mherkazandjian/cce01cf3e15c0b41c1c4321245a99096/raw/03c86dae9a212446cf5b095643854f029b39c921/miniconda_installer.sh | bash -s -- "${target}"/miniconda --upgrade
 
@@ -95,8 +98,8 @@ printf ${delimiter}
 if [[ $(sha256sum sd-v1-4.ckpt | cut -d " " -f 1) != "fe4efff1e174c627256e44ec2991ba279b3816e364b49f9be2abc0b3ff3f8556" ]]
 then
     printf ${delimiter}
-    printf "sd-v1-4.ckpt checksum invalid, this file is not safe !!"
-    printf "Deleting as it can be dangerous"
+    printf "\x1B[31mERROR: sd-v1-4.ckpt checksum invalid, this file is not safe !!\e[0m"
+    printf "\x1B[31mDeleting as it can be dangerous\e[0m"
     printf ${delimiter}
     rm sd-v1-4.ckpt
     exit 1
