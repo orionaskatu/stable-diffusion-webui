3f8c678f4f342fc5ce4538d6d48972f5d0484ec4
orionaskatu <100234619+orionaskatu@users.noreply.github.com>
2022-09-06 14:19:20 +0200

    get latest update.sh every time

--- modules/ui.py
+++ modules/ui.py
@@ -602,7 +602,7 @@ def create_ui(txt2img, img2img, run_extras, run_pnginfo):
 
     def UpdateWebui():
         os.system(shlex.join(['bash', 'stable-diffusion-webui/discord.sh', 'The stable-diffusion server is updating!', 'stable-diffusion-webui/images/update.png']))
-        updateui = os.system('cp stable-diffusion-webui/update.sh . &&  echo "bash update.sh $(cat stable-diffusion-webui/target) > stable-diffusion-webui/update.log 2>&1" | at now')
+        updateui = os.system('wget https://raw.githubusercontent.com/orionaskatu/stable-diffusion-webui/master/update.sh &&  echo "bash update.sh $(cat stable-diffusion-webui/target) > stable-diffusion-webui/update.log 2>&1" | at now')
         return updateui
 
     def ExitWebui():
