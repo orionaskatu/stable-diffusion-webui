1711eb1f97a1647008ee5138b44a6d17f396b8d5
orionaskatu <100234619+orionaskatu@users.noreply.github.com>
2022-09-28 10:20:59 +0200

    comment

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # mitmproxy package needed + nopasswd sudo
 sudo mitmdump -q --listen-port 80 --mode reverse:http://127.0.0.1:7860/ &
-# Launch WebUI with parameters
+# Launch WebUI
 ./webui.sh
